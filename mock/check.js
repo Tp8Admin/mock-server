b5fd0da6b999f77c776c6615dc2c0ff56174cc29
YangHongwei <403236160@qq.com>
2023-08-16 18:18:21 +0800

    数据部分更改

--- mock/check.js
+++ mock/check.js
@@ -5,45 +5,57 @@ module.exports = [
       method: 'get',
       response: () => {
         return {
-          "status": "success",
-          "data": {
+          "code": 1,
+          "data": [
             // 1、PHP的版本
-            php_version: {
+            {
+                name: 'PHP的版本',
+                name_en: 'php_version',
                 describe: "8.2.3",
                 link: [],
                 state: "ok"
             },
             // 2、配置目录
-            config_is_writable: {
+            {
+                name: '配置目录',
+                name_en: 'config_is_writable',
                 describe: "可写",
                 link: [],
                 state: "ok"
             },
             // 3、public目录
-            public_is_writable: {
+             {
+                name: 'public目录',
+                name_en: 'public_is_writable',
                 describe: "可写",
                 link: [],
                 state: "ok"
             },
             // 4、PHP的PDO库
-            php_pdo: {
+            {
+                name: 'PHP的PDO库',
+                name_en: 'php_pdo',
                 describe: "已安装",
                 link: [],
                 state: "ok"
             },
             // 5、PHP的GD2库
-            php_gd2: {
+            {
+                name: 'PHP的GD2库',
+                name_en: 'php_gd2',
                 describe: "已安装",
                 link: [],
                 state: "ok"
             },
             // 6、PHP的proc库
-            php_proc: {
+            {
+                name: 'PHP的proc库',
+                name_en: 'php_proc',
                 describe: "允许执行",
                 link: [],
                 state: "ok"
             },
-          }
+          ]
         }
       }
     },
@@ -54,28 +66,34 @@ module.exports = [
         method: 'get',
         response: () => {
           return {
-            "status": "success",
-            "data": {
+            "code": 1,
+            "data": [
                // 1、NPM版本
-               npm_version: {
+               {
+                name: 'NPM版本',
+                name_en: 'npm_version',
                 describe: "已安装",
                 link: [],
                 state: "ok"
               },
               // 2、NodeJS版本
-              config_is_writable: {
+              {
+                  name: 'NodeJS版本',
+                  name_en: 'config_is_writable',
                   describe: "可写",
                   link: [],
                   state: "ok"
               },
               // 3、NPM包管理器
-              npm_package_manager: {
+              {
+                  name: 'NPM包管理器',
+                  name_en: 'npm_package_manager',
                   describe: "已安装",
                   link: [],
                   state: "ok"
               },
              
-            }
+            ]
           }
         }
       },
